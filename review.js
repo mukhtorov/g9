30b2a617f6a3bfb1adb8389d71bbd8415fd2e112
mukhtorov <mukhtorov_1994@mail.ru>
2023-01-23 12:06:02 +0500

    test

--- review.js
+++ review.js
@@ -1,3 +1,12 @@
 // repository - project
 // 1 - git init
-// 2 - git remote add origin URL
+// 2 - git remote add origin https://github.com/mukhtorov/g9.git
+
+// connection
+// 1. git config -global user.email "webbrain@gmail.com"
+// 2. git config --global user.name "mukhtorov"
+
+// push
+// 1. git add .
+// 2. git commit -m ""
+// 3. git push origin master
